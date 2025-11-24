cef7f56c1e8c3a429b3f6b20fd68afad02ba0f00
owner77777 <nosirovbobowka@gmail.com>
2025-11-24 20:30:38 +0400

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from typing import Union
 # ! Для этого бота я оставил токен здесь, но в реальном проекте используйте os.environ.get('BOT_TOKEN')
 class Config(BaseModel):
     # Замените на os.environ.get('BOT_TOKEN') в реальном проекте
-    BOT_TOKEN: str = "8346884521:AAGvOZdAJA4O3ohHzB2lFI5oTZn3zWyxLY" 
+    BOT_TOKEN: str = "8346884521:AAGvOZdAJA4O3ohHzB2lFI5oTZnz3lWyxLY" 
     OWNER_ID: int = 6493670021
     CHANNEL_PREDLOZHKA_ID: Union[int, str] = -1003287891557
     CHANNEL_FINAL_ID: Union[int, str] = -1003479497567
